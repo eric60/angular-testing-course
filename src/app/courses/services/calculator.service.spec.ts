7eef45070c74832e213eb36abeca6cd746cd9f37
Eric <ericshi98@gmail.com>
2024-07-10 13:08:17 -0400

    Update calculator.service.spec.ts

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,9 +1,20 @@
+import {CalculatorService} from "./calculator.service";
+import {LoggerService} from "./logger.service";
+
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    pending()
+    const calculator = new CalculatorService(new LoggerService())
+
+    const result = calculator.add(1,1)
+
+    expect(result).toBe(2)
   })
 
   it('should subtract two numbers', () => {
-    fail()
+     const calculator = new CalculatorService(new LoggerService())
+
+    const result = calculator.subtract(1,1)
+
+    expect(result).toBe(0, "unexpected subtraction result")
   })
 })
