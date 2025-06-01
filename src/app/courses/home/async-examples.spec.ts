f3a2788f4f8ff4f9f38d1a6d5e2183aa951af58e
Eric <ericshi98@gmail.com>
2025-06-01 15:17:48 -0400

    add async test example plain promise

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -36,6 +36,38 @@ describe('Async Testing Examples', () => {
 
     }));
 
+    fit('Asynchronous test example - plain promise', (() => {
+      let test = false;
+      console.log('Creating promise')
+
+/*      setTimeout(() => {
+        console.log('setTimeout() 1 callback triggered')
+      });
+
+      setTimeout(() => {
+        console.log('setTimeout() 2 callback triggered')
+
+        Creating promise
+context.js:265 Running test assertions
+context.js:265 Promise 1st then() evaluated successfully
+context.js:265 Promise 2nd then() evaluated successfully
+context.js:265 setTimeout() 1 callback triggered
+context.js:265 setTimeout() 2 callback triggered
+      });*/
+
+      Promise.resolve().then(() => {
+        console.log('Promise 1st then() evaluated successfully')
+        return Promise.resolve();
+      }).then(() => {
+        console.log('Promise 2nd then() evaluated successfully')
+          test = true;
+      });
+      // Note: Promise higher priority than setTimeout, 2 separate async tasks in 2 separate queues, browser does microtask queue first
+      // promise is microtask queue in separate queue, browser will not update view in between, lightweight, makes browser more responsive
+      // setTimeout is a macro task queue (setInterval, ajax calls, mouseClicks) added to event loop between each macrotask, browser rendering engine can re-render screen
+      console.log('Running test assertions')
+      expect(test).toBeTruthy()
+    }))
 
     it('Asynchronous test example - plain Promise', fakeAsync(() => {
 
