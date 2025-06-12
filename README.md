4c4d3be9db897d89124c1bfcc5dd7ec893b880bc
Eric <ericshi98@gmail.com>
2025-06-12 07:50:40 -0400

    add cypress ui screenshots

--- README.md
+++ README.md
@@ -56,6 +56,9 @@ To run the frontend part of our code, we will use the Angular CLI:
 
 The application is visible at port 4200: [http://localhost:4200](http://localhost:4200)
 
+# To run cypress ETE tests (vs Integration Tests)
+
+    npm run cypress:open
 
 
 # Important 
--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,19 +1,23 @@
 
 
-
+// 1 terminal running frontend npm start, not running backend (not needed for ete test since going to mock backend responses)
+// 2nd terminal running cypress npm run cypress:open
 describe('Home Page', () => {
 
     beforeEach(() => {
-
         cy.fixture('courses.json').as("coursesJSON");
-
         cy.server();
-
         cy.route('/api/courses', "@coursesJSON").as("courses");
-
         cy.visit('/');
-
     });
+
+    it('should display All Courses header', () => {
+      cy.visit('/');
+      cy.contains("All Courses");
+      // Get the child DOM element that contains given text.
+      // cy.get('.nav').contains('About') // Yield el in .nav containing 'About'
+      // cy.contains('Hello') // Yield first el in document containing 'Hello'
+    })
 
     it('should display a list of courses', () => {
 
