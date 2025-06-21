99690959828767a9ca79ebdb347c03b9aa4f276d
Eric <ericshi98@gmail.com>
2025-06-21 12:31:32 -0400

    update cypress getAdvancesCourses test

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,19 +1,19 @@
 
 
-// 1 terminal running frontend npm start, not running backend (not needed for ete test since going to mock backend responses)
+// 1 terminal running frontend "npm start", not running backend "npm run server" (not needed for ete test since going to mock the backend responses)
 // 2nd terminal running cypress "npm run cypress:open" not "npm run server"
 // ete test: real http request now, but just mocking the backend http request
 describe('Home Page', () => {
 
     beforeEach(() => {
-        // cy.fixture('courses.json').as("coursesJSON"); // courses.json is mock http response
-        // cy.server(); // startup cypress mock http backend server
-        // cy.route('/api/courses', "@coursesJSON").as("courses");
-        // cy.visit('/');
-      cy.wait('@courses')
+        cy.fixture('courses.json').as("coursesJSON"); // courses.json is mock http response
+        cy.server(); // startup cypress mock http backend server
+        cy.route('/api/courses', "@coursesJSON").as("courses");
+        cy.visit('/');
+        cy.wait('@courses')
     });
 
-    it('should display the "All Courses" h3 header even with no backend running', () => {
+    it('should display the "All Courses" h3 header even with no backend running. No need for cy.server() mock http backend server', () => {
       cy.visit('/');
       cy.contains("All Courses");
       cy.get('h3').contains("All Courses")
@@ -22,29 +22,82 @@ describe('Home Page', () => {
       // cy.contains('Hello') // Yield FIRST el in document containing 'Hello'
     })
 
-   /* it('should display a list of courses', () => {
-
+    it('should display a list of courses', () => {
         cy.contains("All Courses");
-
         cy.wait('@courses');
-
         cy.get("mat-card").should("have.length", 9); // 9 material cards
-
     });
 
-    it('should display the advanced courses', () => {
+    it.only('should display the advanced courses', () => {
+        // cy.get('.mat-tab-label').should("have.length", 2);
+        // cy.get('.mat-tab-label').last().click();
 
-        cy.get('.mat-tab-label').should("have.length", 2);
+        cy.get('.mat-mdc-tab').should("have.length", 2);
+        cy.get('.mat-mdc-tab').last().click();
 
-        cy.get('.mat-tab-label').last().click();
+        // get this css class .mat-card-title contained within active container elements containing this other css class .mat-tab-body-active
+      // .mat-tab-body-active .mat-card-title could not be found in the DOM
+        const matTabBodyActiveTitle = '.mat-tab-body-active .mat-card-title';
 
-        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+        let firstMatCardInCoursesList = cy.get('courses-card-list').first();
+        firstMatCardInCoursesList.should('have.length', 1)
+        firstMatCardInCoursesList.get('mat-card-title').should('contain', 'Angular Security Course');
 
-        cy.get('.mat-tab-body-active .mat-card-title').first()
-            .should('contain', "Angular Security Course");
+        // cy.get(matTabBodyActiveTitle).its('length').should('be.gt', 1);
+        // cy.get(matTabBodyActiveTitle).first()
+        //     .should('contain', "Angular Security Course");
 
+        /*
+  <mat-tab-group>
+
+.mat-tab-body-active
+
+    <mat-tab label="Beginners">
+      <courses-card-list (courseChanged)="reload()"
+        [courses]="beginnerCourses$ | async">
+      </courses-card-list>
+    </mat-tab>
+
+    <mat-tab label="Advanced">
+      <courses-card-list  (courseChanged)="reload()"
+        [courses]="advancedCourses$ | async"
+      ></courses-card-list>
+    </mat-tab>
+  </mat-tab-group>
+
+========================================================================
+  <courses-card-list> component below
+
+  <mat-card *ngFor="let course of courses" class="course-card mat-elevation-z10">
+
+    <mat-card-header>
+        <mat-card-title>{{course.titles.description}}</mat-card-title>
+    </mat-card-header>
+
+    <img mat-card-image [src]="course.iconUrl">
+
+    <mat-card-content>
+        <p>{{course.titles.longDescription}}</p>
+    </mat-card-content>
+
+    <mat-card-actions class="course-actions">
+
+        <button mat-raised-button color="primary" [routerLink]="['/courses', course.id]">
+            VIEW COURSE
+        </button>
+
+        <button mat-raised-button color="accent"
+                (click)="editCourse(course)">
+            EDIT
+        </button>
+
+    </mat-card-actions>
+
+</mat-card>
+
+         */
     });
-*/
+
 
 });
 
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -13,7 +13,6 @@ import {sortCoursesBySeqNo} from './sort-course-by-seq';
 export class HomeComponent implements OnInit {
 
     beginnerCourses$: Observable<Course[]>;
-
     advancedCourses$: Observable<Course[]>;
 
     constructor(private coursesService: CoursesService) {
@@ -21,20 +20,13 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-
       this.reloadCourses();
-
     }
 
-
     reloadCourses() {
-
       const courses$ = this.coursesService.findAllCourses();
-
       this.beginnerCourses$ = this.filterByCategory(courses$, 'BEGINNER');
-
       this.advancedCourses$ = this.filterByCategory(courses$, 'ADVANCED');
-
     }
 
     filterByCategory(courses$: Observable<Course[]>, category:string) {
